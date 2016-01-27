dda8401aacb1af7b18611f28b3282251da1f4bab
Daniel Korac <dkorach@lsoc.org>
2016-01-27 14:00:41 -0600

    Completed RPS project based on Pseudocode rps3.rb

---
+++ PseudoRPS.rb
@@ -0,0 +1,44 @@
+wincounter = 0
+losscounter = 0
+3.times do
+	puts "Choose Rock, Paper or Scissors"
+	userinput = gets.chomp
+	userinput = userinput.downcase
+	
+	puts computerinput = ["rock", "paper", "scissors"].sample
+
+	if userinput == computerinput
+		puts "It's a Draw!"
+	elsif userinput == "rock" and computerinput == "scissors"
+		puts "You Win!"
+		wincounter = wincounter + 1 
+	elsif userinput == "paper" and computerinput == "rock"
+		puts "You Win!"
+		wincounter = wincounter + 1 
+	elsif userinput == "scissors" and computerinput == "paper"
+		puts "You Win!"
+		wincounter = wincounter + 1 
+	elsif userinput == "rock" and computerinput == "paper"
+		puts "You Lose!"
+		losscounter = losscounter + 1 
+	elsif userinput == "paper" and computerinput == "scissors"
+		puts "You Lose!"
+		losscounter = losscounter + 1 
+	elsif userinput == "scissors" and computerinput == "rock"
+		puts "You Lose!"
+		losscounter = losscounter + 1 
+	end
+end
+
+if wincounter > losscounter
+	puts "You win the game!"
+elsif losscounter > wincounter
+	puts "You lose the game!"
+elsif losscounter == wincounter
+	puts "You Tied!"
+end 
+	
+
+
+		
+	
